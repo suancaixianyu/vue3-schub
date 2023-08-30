b99ee3772e17482ba3f65ea505027ff9fee7062d
封不觉 <chongsaid@163.com>
2023-08-30 20:27:31 +0800

    Revert "fix:修正列表头像与内容无法对齐的样式"
    This reverts commit 777c91c26314b1276937b58fa2fccc940f2cbe3d.

--- src/components/main/bbs/item.vue
+++ src/components/main/bbs/item.vue
@@ -3,12 +3,6 @@
     <router-link :to="`/postlist/${path}/${item.id}`">
       <el-container>
         <el-aside width="70%">
-          <UserIcon
-              :src="item.author.headurl"
-              :size="headsize"
-              :alt="item.author.nickname"
-              style="margin-right: 12px"
-          />
           <el-text
             class="mx-1 time title"
             size="large"
@@ -22,12 +16,12 @@
           ></el-text>
           <ul class="category-counts">
             <li>
-<!--              <UserIcon-->
-<!--                :src="item.author.headurl"-->
-<!--                :size="headsize"-->
-<!--                :alt="item.author.nickname"-->
-<!--                style="margin-right: 12px"-->
-<!--              />-->
+              <UserIcon
+                :src="item.author.headurl"
+                :size="headsize"
+                :alt="item.author.nickname"
+                style="margin-right: 12px"
+              />
               <el-text size="small">{{ item.time }}</el-text>
             </li>
 
