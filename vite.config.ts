4bc298330d73707a8d01467ec0a500ea484a733b
suancaixianyu <3501869534@qq.com>
2023-11-18 20:37:31 +0800

    修改pwa配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,32 @@ import { resolve } from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue(),
-  VitePWA({
-    manifest: {
-      // 安装应用后显示的应用名
-      name: "生存战争中文社区",
-      description: "生存战争中文社区",
-      // 至少配置一个图标
-      icons: [{
-        // 注意如果应用不是部署在站点根目录则需要相对路径，图片文件放在项目/public/pwa/192x192.png
-        src: "./pwa/icon.svg",
-        sizes: "192x192",
-        type: "image/svg+xml"
-      }]
-    },
-    registerType: "autoUpdate",
-    workbox: {
-      // 对所有匹配的静态资源进行缓存
-      globPatterns: ["**/*.{js,css,html,ico,png,jpeg,jpg,svg}"],
-    },
-    devOptions: {
-      enabled: true
-    }
-  })
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        // 安装应用后显示的应用名
+        name: '生存战争中文社区',
+        short_name: '生存战争中文社区',
+        description: '生存战争中文社区',
+        // 至少配置一个图标
+        icons: [
+          {
+            // 注意如果应用不是部署在站点根目录则需要相对路径，图片文件放在项目/public/pwa/192x192.png
+            src: './pwa/icon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+        ],
+        background_color: '#ffffff',
+        theme_color: '#42b883',
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        // 对所有匹配的静态资源进行缓存
+        globPatterns: ['**/*.{js,css,html,ico,png,jpeg,jpg,svg}'],
+      },
+    }),
   ],
   resolve: {
     alias: {
