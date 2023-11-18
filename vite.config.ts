59cb516f3de06d6b7720865319ea30b4c426d597
suancaixianyu <3501869534@qq.com>
2023-11-18 20:20:32 +0800

    更新依赖，添加pwa支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+  VitePWA({
+    manifest: {
+      // 安装应用后显示的应用名
+      name: "生存战争中文社区",
+      description: "生存战争中文社区",
+      // 至少配置一个图标
+      icons: [{
+        // 注意如果应用不是部署在站点根目录则需要相对路径，图片文件放在项目/public/pwa/192x192.png
+        src: "./pwa/icon.svg",
+        sizes: "192x192",
+        type: "image/svg+xml"
+      }]
+    },
+    registerType: "autoUpdate",
+    workbox: {
+      // 对所有匹配的静态资源进行缓存
+      globPatterns: ["**/*.{js,css,html,ico,png,jpeg,jpg,svg}"],
+    },
+    devOptions: {
+      enabled: true
+    }
+  })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
